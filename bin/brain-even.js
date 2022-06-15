28a7cfa4dc36226ce6204bc05e12b45d4d2b79ad
woomlg <pashkarabchonok@gmail.com>
2022-06-15 22:07:30 +0300

    add even game

---
+++ bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import {name, getEven} from '../src/evenGame.js';
+
+console.log(`Hello, ${name}`)
+
+getEven();
---
+++ src/evenGame.js
@@ -0,0 +1,35 @@
+import readlineSync from 'readline-sync';
+
+export const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?: ');
+
+// Функция получения целого, случайного числа
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
+
+// Реализация игры проверка на четсность 
+export const getEven = () => {
+
+    console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+    for (let i = 0; i < 3; i += 1) {
+        // Выводим число на экран
+        const num = getRandomNumber(20);
+        console.log(`Question: ${num}`);
+
+        // Ответ игрока
+        const getAnswerUser = readlineSync.question('Your answer: ');
+
+        // Определяем чет/нечет
+        const evenNum = (num % 2 === 0) ? 'yes' : 'no';
+
+        // Выполняем проверку 
+        if (getAnswerUser === evenNum) {
+            console.log('Correct!');
+        } else {
+            console.log(`"${getAnswerUser}" is wrong answer ;(. Correct answer was "${evenNum}"`);
+            return console.log(`Let's try again ${name}`);
+        }
+        
+    }
+
+    return console.log(`Congratulations, ${name}`);
+};
