c3001a0e6cbc84029e16ac2ce9087f7c40f87356
woomlg <pashkarabchonok@gmail.com>
2022-06-16 23:29:45 +0300

    push game logic into games dir

---
+++ games/evenGame.js
@@ -0,0 +1,19 @@
+import getRandomNumber from '../src/randomNumber.js';
+import gameBasic from '../src/index.js';
+
+const getEven = () => {
+    
+    const gameTask = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+    const gameData = () => {
+      const randomNumber = getRandomNumber(100);
+
+      const rightAnswer = (randomNumber % 2 === 0) ? 'yes' : 'no';
+
+      return [randomNumber, rightAnswer];
+    }
+
+    gameBasic(gameTask, gameData);
+};
+
+export default getEven;
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import readlineSync from 'readline-sync';
+
+// приветствие
+const gameBasic = (gameRule, gameData) => {
+    const name = readlineSync.question('Welcome to the Brain Games!\nMay I have your name?: ');
+    console.log(`Hello, ${name}!`);
+
+    console.log(gameRule);
+
+    for (let i = 0; i < 3; i += 1) {
+        
+        const gameElements = gameData();
+
+        console.log(`Question: ${gameElements[0]}`)
+
+        const getAnswer = readlineSync.question('Your answer: ');
+
+        const theAnswer = gameElements[1];
+
+        if (getAnswer === theAnswer) {
+            console.log('Correct!');
+        } else {
+            console.log(`"${getAnswer}" is wrong answer ;(. Correct answer was "${theAnswer}"`);
+
+            return console.log(`Let's try again ${name}`);
+        }
+    };
+
+    return console.log(`Congratulations, ${name}`);
+};
+
+export default gameBasic;
