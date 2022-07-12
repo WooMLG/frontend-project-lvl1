7bd4a60418743e6fe2dcead84053f4ec28d14d97
woomlg <pashkarabchonok@gmail.com>
2022-07-12 18:57:34 +0300

    fixed brain-progression (progression length)

--- src/getRandomProgression.js
+++ src/getRandomProgression.js
@@ -2,7 +2,7 @@ import getRandomNumber from './randomNumber.js';
 
 const getRandomProgression = () => {
   const stepOfProgression = 1 + getRandomNumber(9);
-  const lengthOfProgression = (stepOfProgression < 4) ? 4 : stepOfProgression;
+  const lengthOfProgression = (stepOfProgression < 6) ? 6 : stepOfProgression;
   const result = [getRandomNumber(20)];
   for (let i = 0; i < lengthOfProgression; i += 1) {
     result.push(result[i] + stepOfProgression);
